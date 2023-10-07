e4e00c1f4e612953284b3f20a4aa0c17f538a0c0
Lukas Marienfeld <lukasmarienfeldlm@gmail.com>
2023-10-07 22:54:45 +0200

    Add trpc router

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,60 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { likes } from "@/server/db/schema";
+import { and, eq } from "drizzle-orm";
+import { z } from "zod";
+
+export const userRouter = createTRPCRouter({
+  getLikes: protectedProcedure.query(({ ctx }) => {
+    return ctx.db
+      .select()
+      .from(likes)
+      .where(eq(likes.userId, ctx.session.user.id))
+      .execute();
+  }),
+  toggleLike: protectedProcedure
+    .input(
+      z.object({
+        entityId: z.number(),
+        entityType: z.enum(["character", "episode", "location"]),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const like = await ctx.db
+        .select()
+        .from(likes)
+        .where(
+          and(
+            eq(likes.userId, ctx.session.user.id),
+            eq(likes.entityId, input.entityId),
+            eq(likes.entityType, input.entityType),
+          ),
+        )
+        .limit(1)
+        .execute();
+
+      if (like.length > 0) {
+        await ctx.db
+          .delete(likes)
+          .where(
+            and(
+              eq(likes.userId, ctx.session.user.id),
+              eq(likes.entityId, input.entityId),
+              eq(likes.entityType, input.entityType),
+            ),
+          )
+          .execute();
+      } else {
+        await ctx.db
+          .insert(likes)
+          .values({
+            userId: ctx.session.user.id,
+            entityId: input.entityId,
+            entityType: input.entityType,
+          })
+          .execute();
+      }
+      return {
+        success: true,
+      };
+    }),
+});
